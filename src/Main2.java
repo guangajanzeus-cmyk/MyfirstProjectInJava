2320d97fc990fa23b52292d51b7dfd2dd0cf087a
guangajanzeus-cmyk <guangajanzeus@gmail.com>
2026-07-07 20:43:21 +0800

    Project

--- src/Main2.java
+++ src/Main2.java
@@ -13,7 +13,7 @@ public class Main2 {
         //boolean isStudent;
 
         //System.out.print("\nHow old are you: ");
-       // age = scanner.nextInt();
+        // age = scanner.nextInt();
 
         //System.out.print("Are you a student from TCNHS: ");
         //isStudent = scanner.nextBoolean();
@@ -27,34 +27,34 @@ public class Main2 {
         //Group 1
 
         //if (name.isEmpty()) {
-            //System.out.println("\nYou DIDN'T enter your name! 😡");
+        //System.out.println("\nYou DIDN'T enter your name! 😡");
         //} else {
-            //System.out.println("\nHello " + name + " 😉");
+        //System.out.println("\nHello " + name + " 😉");
         //}
 
         //Group 2
 
         //if (age >= 65) {
-            //System.out.println("You're a senior!👴");
+        //System.out.println("You're a senior!👴");
         //} else if (age >= 18) {
-            //System.out.println("You're a adult! 🧑");
+        //System.out.println("You're a adult! 🧑");
         //} else if (age == 0) {
-            //System.out.println("You are a baby! 👶");
+        //System.out.println("You are a baby! 👶");
         //} else if (age < 0) {
-            //System.out.println("You haven't born yet 😇");
+        //System.out.println("You haven't born yet 😇");
         //} else {
-            //System.out.println("You're a teenager! 👦");
+        //System.out.println("You're a teenager! 👦");
         //}
 
         //Group 3
 
         //if (isStudent) {
-           // System.out.println("You're a Student! 🏫");
+        // System.out.println("You're a Student! 🏫");
         //} else {
-            //System.out.println("You're NOT a student from TCNHS! 🚫");
-       // }
+        //System.out.println("You're NOT a student from TCNHS! 🚫");
+        // }
 
-       // scanner.close();
+        // scanner.close();
 
         //Random random = new Random();
 
@@ -72,40 +72,40 @@ public class Main2 {
 
         //boolean isHeads;
 
-       // isHeads = random.nextBoolean();
+        // isHeads = random.nextBoolean();
 
         //if (isHeads) {
-           // System.out.println("Heads");
+        // System.out.println("Heads");
         //}
         //else {
-           // System.out.println("Tails!");
+        // System.out.println("Tails!");
         //}
 
-        //String name = "Password";
+        String name = "Password";
 
         //int length   = name.length();
         //char letter = name.charAt(0);
-       // int index = name.indexOf(" ");
+        // int index = name.indexOf(" ");
         //int lastIndex = name.lastIndexOf("Z");
 
         //name = name.toUpperCase();
         //ame = name.toLowerCase();
         //name = name.trim();
-       // name = name.replace("a", "e");
+        // name = name.replace("a", "e");
 
-        //if (name.isEmpty()){
-           // System.out.println("Your Name is Empty");
-        //}
-       // else {
-            //System.out.println("Hello " + name);
-        //}
-       //if (name.equalsIgnoreCase("password")){
-           // System.out.println("Your name can't be password");
-        //}
-       // else {
-            //ystem.out.println("Hello " + name);
-       // }
+        if (name.isEmpty()){
+         System.out.println("Your Name is Empty");
+        }
+         else {
+        System.out.println("Hello " + name);
+        }
+        if (name.equalsIgnoreCase("password")){
+         System.out.println("Your name can't be password");
+    }
+       else {
+            System.out.println("Hello " + name);
+       }
 
-        //System.out.println(name);
+        System.out.println(name);
     }
 }
